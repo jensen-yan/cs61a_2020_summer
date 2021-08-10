08e55873ca7f6870ded951baf7c665a88f50c950
jensen-yan <yanyue171@mails.ucas.ac.cn>
2021-08-10 16:36:51 +0800

    finish lab08

--- lab/lab08 /lab08/classes.py
+++ lab/lab08 /lab08/classes.py
@@ -23,6 +23,9 @@ class Card:
         500
         """
         "*** YOUR CODE HERE ***"
+        self.name = name
+        self.attack = attack
+        self.defense = defense
 
     def power(self, other_card):
         """
@@ -42,6 +45,7 @@ class Card:
         50.0
         """
         "*** YOUR CODE HERE ***"
+        return self.attack - other_card.defense / 2
 
 
     def effect(self, other_card, player, opponent):
@@ -80,6 +84,7 @@ class Player:
         self.deck = deck
         self.name = name
         "*** YOUR CODE HERE ***"
+        self.hand = [deck.draw() for _ in range(5)]
 
     def draw(self):
         """Draw a card from the player's deck and add it to their hand.
@@ -94,6 +99,7 @@ class Player:
         """
         assert not self.deck.is_empty(), 'Deck is empty!'
         "*** YOUR CODE HERE ***"
+        self.hand += [self.deck.draw()]
 
     def play(self, card_index):
         """Remove and return a card from the player's hand at the given index.
@@ -110,6 +116,7 @@ class Player:
         2
         """
         "*** YOUR CODE HERE ***"
+        return self.hand.pop(card_index)
 
     def display_hand(self):
         """
@@ -150,8 +157,12 @@ class TutorCard(Card):
         True
         """
         "*** YOUR CODE HERE ***"
+        # 对方丟3张牌, 拿3张牌
+        opponent.hand = opponent.hand[3:]
+        for _ in range(3):
+            opponent.draw()
         #Uncomment the line below when you've finished implementing this method!
-        #print('{} discarded and re-drew 3 cards!'.format(opponent.name))
+        print('{} discarded and re-drew 3 cards!'.format(opponent.name))
 
     def copy(self):
         """
@@ -176,6 +187,7 @@ class TACard(Card):
         300
         """
         "*** YOUR CODE HERE ***"
+        other_card.attack, other_card.defense = other_card.defense, other_card.attack
 
     def copy(self):
         """
@@ -206,10 +218,19 @@ class ProfessorCard(Card):
         """
         orig_opponent_deck_length = len(opponent.deck.cards)
         "*** YOUR CODE HERE ***"
+        # 把opponent的攻防都加到自己牌堆的每张牌中.
+        for card in player.deck.cards:
+            card.attack  += other_card.attack
+            card.defense += other_card.defense
+        # 对方牌堆的每张牌, 如果攻or防和opponent的攻防相同, 删除牌
+        for card in opponent.deck.cards[:]:
+            if card.attack == other_card.attack or card.defense == other_card.defense:
+                opponent.deck.cards.remove(card)
+
         discarded = orig_opponent_deck_length - len(opponent.deck.cards)
         if discarded:
             #Uncomment the line below when you've finished implementing this method!
-            #print('{} cards were discarded from {}\'s deck!'.format(discarded, opponent.name))
+            print('{} cards were discarded from {}\'s deck!'.format(discarded, opponent.name))
             return
 
     def copy(self):
--- lab/lab08 /lab08/lab08.py
+++ lab/lab08 /lab08/lab08.py
@@ -33,4 +33,51 @@ def make_generators_generator(g):
     9
     """
     "*** YOUR CODE HERE ***"
+    # 好难啊, 都被绕晕了...
+    # 一个generator 函数, 根据次数 生成数据
+    def gen_helper(num_next):
+        gen = g()
+        for i in range(num_next):
+            yield next(gen)
 
+    num = len(list(g()))
+    for i in range(1, num+1):
+        yield gen_helper(i)     # 返回多个generator 函数
+
+    # yield gen_helper(1)
+    # yield gen_helper(2)
+    # yield gen_helper(3)
+    # yield gen_helper(4)
+
+    # gen = g()   # generator object
+    # yield gen
+
+    # gen = g()
+    # next(gen)
+    # yield gen
+
+    # gen = g()
+    # next(gen)
+    # next(gen)
+    # yield gen
+
+
+    # def ge():
+    #     for item in g:
+    #         yield item
+    # yield ge()
+
+# def every_m_ints_to(n, m):
+#     i = 0
+#     while (i <= n):
+#         yield i
+#         i += m
+
+# def every_3_ints_to_10():
+#     for item in every_m_ints_to(10, 3):
+#         yield item
+
+# for gen in make_generators_generator(every_3_ints_to_10):
+#     print("Next Generator:")
+#     for item in gen:
+#         print(item)
